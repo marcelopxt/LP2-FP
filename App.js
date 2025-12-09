e0848026beeb68fe890397980c2924b0e79cf192
Primeiro pc da Primeira Fileira <pc1dafileira1@gmail.com>
2025-12-09 12:00:53 -0300

    adiciona bottomtabsNavigation

--- App.js
+++ App.js
@@ -1,33 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Header from './src/components/Header';
-import Footer from './src/components/Footer';
-import { SafeAreaView } from 'react-native-web';
-import Home from './src/screens/Home';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import Header from "./src/components/Header";
+import Home from "./src/screens/Home";
+import Edit from "./src/screens/Edit";
+import Show from "./src/screens/Show";
+import Create from "./src/screens/Create";
 
 export default function App() {
+  const Tab = createBottomTabNavigator();
+
   return (
-    <SafeAreaView style={styles.area}>
-      <Header/> 
-      <View style={styles.conteudo}>
-        <Home></Home>
-      </View>
-      <Footer/> 
-    </SafeAreaView>
+    <View style={styles.container}>
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={{
+            header: () => <Header />,
+            tabBarStyle: { paddingBottom: 5, paddingTop: 5, height: 60 },
+            tabBarActiveTintColor: "blue",
+            tabBarInactiveTintColor: "gray",
+          }}
+        >
+          <Tab.Screen
+            name="Home"
+            component={Home}
+            options={{ tabBarLabel: "Início" }}
+          />
+          <Tab.Screen
+            name="Create"
+            component={Create}
+            options={{ tabBarLabel: "Cadastrar" }}
+          />
+          <Tab.Screen
+            name="Show"
+            component={Show}
+            options={{ tabBarLabel: "Visualizar" }}
+          />
+          <Tab.Screen
+            name="Edit"
+            component={Edit}
+            options={{ tabBarLabel: "Editar" }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  conteudo: {
+  container: {
     flex: 1,
-    width: '100%',
-    backgroundColor: '#bebebeff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  area: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,48 @@
-import { StyleSheet } from "react-native";
-import { Text } from "react-native";
-import { View } from "react-native-web";
-
+import { useNavigation } from "@react-navigation/native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native"; 
 export default function Header() {
+  const navigation = useNavigation()
   return (
-    <>
-      <View style={styles.container}>
-        <Text style={styles.logo}>Logo</Text>
-        <Text style={styles.title}> Title Here </Text>
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.leftContainer}
+         onPress={()=>{ navigation.navigate('Home') }}>
+        <Text style={styles.logoText}>Logo</Text>
+        </TouchableOpacity>
+
+      <View style={styles.titleContainer}>
+        <Text style={styles.titleText}>Title Here</Text>
       </View>
-    </>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    width: '100%',
+    height: 80,
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 15,
     paddingTop: 25,
-    paddingBottom: 25,
   },
-  logo:{
-    fontSize: 30
+  leftContainer: {
+    zIndex: 1,
   },
-  title:{
-    fontSize: 40
+  logoText: {
+    fontWeight: 'bold',
+  },
+  titleContainer: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 25,
+    bottom: 0,
+    justifyContent: 'center', 
+    alignItems: 'center',
+    zIndex: -1,
+  },
+  titleText: {
+    fontSize: 26,
+    fontWeight: '600',
   }
 });
